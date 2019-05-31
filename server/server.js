9dff87d9adb0e1f0adc6afa621fac6df963b8753
Sebastian Gabl <sebastian.gabl@gmx.at>
2019-05-31 15:12:25 +0200

    Improve design, format code

--- server/server.js
+++ server/server.js
@@ -1,45 +1,44 @@
 const WebSocketServer = require("websocket").server;
 const http = require("http");
 
-const server = http.createServer(function(request, response) {
-    console.log("Received request for " + request.url);
-    response.writeHead(404);
-    response.end();
+const server = http.createServer(function (request, response) {
+  console.log("Received request for " + request.url);
+  response.writeHead(404);
+  response.end();
 });
-server.listen(8080, function() {
-    console.log("Server is listening on port 8080");
+server.listen(8080, function () {
+  console.log("Server is listening on port 8080");
 });
 
 wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false
+  httpServer: server,
+  autoAcceptConnections: false
 });
 
 function originIsAllowed(origin) {
-    // put logic here to detect whether the specified origin is allowed.
-    return true;
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
 }
 
-wsServer.on("request", function(request) {
-    if (!originIsAllowed(request.origin)) {
-        request.reject();
-        console.log("Connection from origin " + request.origin + " rejected.");
-        return;
+wsServer.on("request", function (request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    console.log("Connection from origin " + request.origin + " rejected.");
+    return;
+  }
+
+  const connection = request.accept("echo-protocol", request.origin);
+  connection.sendUTF("Hello World!");
+
+  console.log("Connection from origin " + request.origin + " accepted.");
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      console.log("Received Message: " + message.utf8Data);
+    } else if (message.type === "binary") {
+      console.log("Received Binary Message of " + message.binaryData.length + " bytes");
     }
-
-    const connection = request.accept("echo-protocol", request.origin);
-    connection.sendUTF("Hello World!");
-
-    console.log("Connection from origin " + request.origin + " accepted.");
-    connection.on("message", function(message) {
-        if (message.type === "utf8") {
-            console.log("Received Message: " + message.utf8Data);
-        }
-        else if (message.type === "binary") {
-            console.log("Received Binary Message of " + message.binaryData.length + " bytes");
-        }
-    });
-    connection.on("close", function(reasonCode, description) {
-        console.log("Peer " + connection.remoteAddress + " disconnected.");
-    });
+  });
+  connection.on("close", function (reasonCode, description) {
+    console.log("Peer " + connection.remoteAddress + " disconnected.");
+  });
 });
